d18695f73ea9860d5357b0f79486f7189ead7be7
Renan Alexandre Berton <89887498+renanberton@users.noreply.github.com>
2021-10-28 17:32:12 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-new SimpleSlide({
-    slide: 'principal', // nome do atributo data-slide="principal"
-    auto: true, // se o slide deve passar automaticamente
-    time: 3500, // tempo de transição dos slides
-})
