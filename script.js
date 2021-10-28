e4664d7733d626959fc6cf32d1a2a32161aed945
Renan Alexandre Berton <89887498+renanberton@users.noreply.github.com>
2021-10-28 17:31:20 -0300

    Animações em JS

---
+++ script.js
@@ -0,0 +1,5 @@
+new SimpleSlide({
+    slide: 'principal', // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 3500, // tempo de transição dos slides
+})
